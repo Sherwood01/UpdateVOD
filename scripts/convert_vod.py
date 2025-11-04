6916df873aab9107da4c710e1804964d4e42eddc
Sherwood <68983530+Sherwood01@users.noreply.github.com>
2025-11-04 14:34:06 +0800

    Update convert_vod.py

--- scripts/convert_vod.py
+++ scripts/convert_vod.py
@@ -10,7 +10,8 @@ VOD_SOURCES = [
     {"url": "http://vod.xwqin.com/output/20.json", "output": "20.json", "convert": False},
     {"url": "http://vod.xwqin.com/output/32.json", "output": "32.json", "convert": False},
     {"url": "http://vod.xwqin.com/output/98.json", "output": "98.json", "convert": False},
-    {"url": "http://vod.xwqin.com/output/118.json", "output": "118.json", "convert": False}
+    {"url": "http://vod.xwqin.com/output/118.json", "output": "118.json", "convert": False}，
+    {"url": "https://raw.githubusercontent.com/hafrey1/LunaTV-config/refs/heads/main/LunaTV-config.json", "output": "all.json", "convert": False}
 ]
 
 def convert_vod(vods):
